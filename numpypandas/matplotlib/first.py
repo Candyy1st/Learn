f58e0bbe585669f3a7f622e74b4958e83654505e
Candyy1st <candradewa1981@gmail.com>
2020-12-09 18:16:23 +0700

    add first file for matplotlib

--- numpypandas/matplotlib/first.py
+++ numpypandas/matplotlib/first.py
@@ -10,4 +10,25 @@ a = plt.plot(x,y)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('Testing Matplotlib')
-print(a)
+
+b = plt.subplot(3,2,1)
+plt.plot(x,x)       #line
+plt.title('Line')
+c = plt.subplot(3,2,2)
+plt.scatter(x,z)    #dot
+plt.title('Dot')
+d = plt.subplot(3,2,3)
+plt.hist(x,y)       #histogram
+plt.title('Histogram')
+e = plt.subplot(3,2,4)
+plt.bar(x,y)
+plt.title('Bar Graph')
+f = plt.subplot(3,2,5)
+plt.fill(x,y)
+plt.title('Fill')
+plt.tight_layout()
+print(b)
+print(c)
+print(d)
+print(e)
+
